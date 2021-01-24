339185b0b4d909caf0337d6561649c1d43bf0d55
Saidabdulla <abdullamasharipov11@gmail.com>
2021-01-24 21:43:38 +0500

    Added typed js

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+let typed = new Typed('#typed', {
+  strings: ['salom aleykum', 'va aleykum assalom'],
+  typeSpeed: 100,
+  backSpeed: 30,
+  fadeOut: false,
+  loop: true,
+  backDelay: 1000
+});
+  
