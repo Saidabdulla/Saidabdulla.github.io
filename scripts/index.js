2bba6aececa74553fad803f207a9018602adc5a8
Saidabdulla <abdullamasharipov11@gmail.com>
2021-01-25 14:40:25 +0500

    Edited typed js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
-let typed = new Typed('#typed', {
-  strings: ['salom aleykum', 'va aleykum assalom'],
-  typeSpeed: 100,
+let typed = new Typed('#typing', {
+  strings: ['Hi there <span>&#128075;</span>', "I'm Saidabdulla, web developer from Uzbekistan, Khorezm.", "I like to do amazing web applications.", "I deliver quality and user-friendly applications for your business."],
+  typeSpeed: 80,
   backSpeed: 30,
   fadeOut: false,
   loop: true,
-  backDelay: 1000
+  backDelay: 1000,
+  showCursor: false,
+  // character for cursor
+  cursorChar: "|",
+  // attribute to type (null == text)
+  attr: null,
+  // either html or text
+  contentType: 'html'
 });
   
