4474c9e89d77ea6c48a56b639e2b296a34785129
Saidabdulla <abdullamasharipov11@gmail.com>
2021-01-27 19:23:37 +0500

    Edited some files

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,22 @@
-let typed = new Typed('#typing', {
-  strings: ['Hi there <span>&#128075;</span>', "I'm Saidabdulla, web developer from Uzbekistan, Khorezm.", "I like to do amazing web applications.", "I deliver quality and user-friendly applications for your business."],
-  typeSpeed: 80,
-  backSpeed: 30,
-  fadeOut: false,
-  loop: true,
-  backDelay: 1000,
-  showCursor: false,
-  // character for cursor
-  cursorChar: "|",
-  // attribute to type (null == text)
-  attr: null,
-  // either html or text
-  contentType: 'html'
-});
-  
+setTimeout(() => {
+
+  document.getElementById('typing').style.display = 'block';
+
+  let typed = new Typed('#typing', {
+    strings: ['Hi there <span>&#128075;</span>', "I'm Saidabdulla, web developer from Uzbekistan, Khorezm.", "I like to do amazing web applications.", "I deliver quality and user-friendly applications for your business."],
+    typeSpeed: 80,
+    backSpeed: 30,
+    fadeOut: false,
+    loop: true,
+    backDelay: 1000,
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html'
+  });
+
+    
+}, 1100);
